9cab13931e7330bc4e90eeaf849347b8652b923c
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-08-22 12:52:03 -0400

    Good stuff is percolating

--- sciaArtifactID/src/main/java/gui/ProcessGraphDBController.java
+++ sciaArtifactID/src/main/java/gui/ProcessGraphDBController.java
@@ -28,6 +28,7 @@ import edu.UC.PhD.CodeProject.nicholdw.query.QueryTables;
 import edu.UC.PhD.CodeProject.nicholdw.queryParserANTLR4.QueryParser;
 import edu.UC.PhD.CodeProject.nicholdw.queryType.QueryTypeSelect;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -61,7 +62,7 @@ public class ProcessGraphDBController {
 	@FXML	private TextArea txaGraphDBFilePath, txaDBResults;
 	@FXML	private TextArea txaGraphDB01FilePath, txaDB01Results, txaGraphDB02FilePath, txaDB02Results;
 	@FXML	private Button btnDBSubmit, btnDBBrowse, btnDBCompare, btnDB01Browse, btnDB02Browse;
-	@FXML 	private Label lblDB01UnmatchedNodes, lblDB02UnmatchedNodes, lblResults;
+	@FXML 	private Label lblDB01UnmatchedNodes, lblDB02UnmatchedNodes, lblResults, lblWorking;
 	@FXML
 	private void initialize() { // Automagically called by JavaFX
 		Log.logProgress("ProcessGraphDBController.Initialize() starting...");
@@ -74,6 +75,7 @@ public class ProcessGraphDBController {
 	}
 	private void setTheScene() {
 		displayResultsControls(false);
+		lblWorking.setVisible(false);
 	}
 	private void displayResultsControls(boolean visible) {
 		lblDB01UnmatchedNodes.setVisible(visible);
@@ -97,35 +99,51 @@ public class ProcessGraphDBController {
 		txaDB02Results.clear();
 	}
 	private void CompareDB() {
+		Neo4jNodes neo4jNodes01 = new Neo4jNodes();
+		Neo4jNodes neo4jNodes02 = new Neo4jNodes();
 		btnDBCompare.setVisible(false);
 		btnDBCompare.setDisable(true);
 		clearResults();
 		displayResultsControls(false);
-		try {
-		try {Thread.sleep(1000);} catch (Exception ex) {}
-		Neo4jNodes neo4jNodes01 = new Neo4jNodes();
-		Neo4jNodes neo4jNodes02 = new Neo4jNodes();
-		boolean result = Neo4jDB.compareDatabases(txaGraphDB01FilePath.getText(), txaGraphDB02FilePath.getText(), false, neo4jNodes01, neo4jNodes02);
-		if (result == true) {
-			//lblResults.setStyle("-fx-background-color:green; -fx-font-color:white;");
-			lblResults.setText("The graphs are equivalent.");
-		} else {
-			//lblResults.setStyle("-fx-background-color:red; -fx-font-color:white;");
-			lblResults.setText("The graphs are not equivalent.");
-		}
-		for (Neo4jNode neo4jNode: neo4jNodes01.getNeo4jNodes()) {
-			txaDB01Results.appendText(neo4jNode.toString() + System.getProperty("line.separator"));
-		}
-		for (Neo4jNode neo4jNode: neo4jNodes02.getNeo4jNodes()) {
-			txaDB02Results.appendText(neo4jNode.toString() + System.getProperty("line.separator"));
-		}
-		} catch (Exception ex) {
-			Log.logError("ProcessGraphDBController.CompareDB(): " + ex.getLocalizedMessage());
-		} finally {
+		lblWorking.setVisible(true);
+		// See https://stackoverflow.com/questions/19968012/javafx-update-ui-label-asynchronously-with-messages-while-application-different/19969793#19969793
+	    Task <Void> task = new Task<Void>() {
+	        @Override public Void call() throws InterruptedException {
+	        	// Do not access any controls in here. An exception will be thrown. It's ugly.
+	    		try {
+	    			Boolean result = Neo4jDB.compareDatabases(txaGraphDB01FilePath.getText(), txaGraphDB02FilePath.getText(), false, neo4jNodes01, neo4jNodes02);
+	    		} catch (Exception ex) {
+	    			Log.logError("ProcessGraphDBController.CompareDB(): " + ex.getLocalizedMessage());
+	    		} finally {
+	    		}
+	        	return null;
+	        }
+	    };
+	    task.setOnSucceeded(e -> {
+			lblWorking.setVisible(false);
 			displayResultsControls(true);
 			btnDBCompare.setVisible(true);
 			btnDBCompare.setDisable(false);
-		}
+			if (neo4jNodes01.countUnmatchedNodes() == 0 && neo4jNodes02.countUnmatchedNodes() == 0) {
+				//lblResults.setStyle("-fx-background-color:green; -fx-font-color:white;");
+				lblResults.setText("The graphs are equivalent.");
+			} else {
+				//lblResults.setStyle("-fx-background-color:red; -fx-font-color:white;");
+				lblResults.setText("The graphs are not equivalent.");
+			}
+			for (Neo4jNode neo4jNode: neo4jNodes01.getNeo4jNodes()) {
+				txaDB01Results.appendText(neo4jNode.toString() + System.getProperty("line.separator"));
+			}
+			for (Neo4jNode neo4jNode: neo4jNodes02.getNeo4jNodes()) {
+				txaDB02Results.appendText(neo4jNode.toString() + System.getProperty("line.separator"));
+			}
+	      });
+	    
+	    // Do the work in a separate thread
+	    Thread thread = new Thread(task);
+	    thread.setDaemon(true);
+	    thread.start();	    
+    
 	}
 	@FXML
 	private void btnDBBrowse_OnClick(ActionEvent event) {
